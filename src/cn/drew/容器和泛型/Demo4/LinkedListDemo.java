189dc4ae73e8301db33205a9b5fbaeb15f0967bf
zlljock <1428567457@qq.com>
2019-05-09 23:30:16 +0800

    第四题

---
+++ src/cn/drew/容器和泛型/Demo4/LinkedListDemo.java
@@ -0,0 +1,48 @@
+package cn.drew.容器和泛型.Demo4;
+
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+
+
+
+        LinkedList linkedList=new LinkedList();
+
+        //进栈
+        public void  push(Object value){
+            linkedList.addFirst(value);
+        }
+
+        //出栈
+        public Object pop(){//弹
+            Object value=linkedList.getFirst();
+            linkedList.removeFirst();
+            return value;
+        }
+
+        //容器大小
+        public int size(){
+            return linkedList.size();
+        }
+
+        public static void main(String[] args) {
+            LinkedListDemo lld = new LinkedListDemo();
+            lld.push("1");
+            lld.push("2");
+            lld.push("3");
+            lld.push("4");
+            lld.push("5");
+
+            while(lld.size()!=0){
+                System.out.println(lld.pop());
+            }
+
+        }
+    }
+
+
+
+
+
+
